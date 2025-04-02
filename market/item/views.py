d428e0a028ec2b28e13b7ee88ed56885d4249907
Moses Valladares <mvallad1@binghamton.edu>
2025-04-01 23:34:58 -0400

    finished delete functionality and added ability to edit item info and search for items using keywords

--- market/item/views.py
+++ market/item/views.py
@@ -1,10 +1,25 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
-from .models import Item
+from .forms import NewItemForm, EditItemForm
+from .models import Category, Item
 
 # Create your views here.
+
+def items(request):
+    query = request.GET.get('query','')
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items' : items,
+        'query' : query,
+        'categories' : categories,
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -43,3 +58,23 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+            
+            return redirect('item:detail', pk=item.id)
+    
+    else:
+        form = EditItemForm(instance=item)
+        
+    return render(request, 'item/form.html', {
+        'form' : form,
+        'title' : 'Edit item',
+    })
